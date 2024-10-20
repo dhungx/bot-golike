8026a562118d51d92af786709c791720a6dc674f
dhungx <139793488+dhungx@users.noreply.github.com>
2024-10-20 21:28:05 +0700

    Update install_packages.py

--- install_packages.py
+++ install_packages.py
@@ -4,13 +4,13 @@ import subprocess
 def install_packages():
     packages = ["playwright", "recognizer", "asyncio"]
     try:
-        subprocess.run(["pip", "install", "--upgrade", "pip"], check=True)
+        subprocess.run(["pip3", "install", "--upgrade", "pip3"], check=True)
         print("Successfully upgraded pip")
     except subprocess.CalledProcessError as e:
         print(f"Failed to upgrade pip. Error: {e}")
     for package in packages:
         try:
-            subprocess.run(["pip", "install", package], check=True)
+            subprocess.run(["pip3", "install", package], check=True)
             print(f"Successfully installed {package}")
         except subprocess.CalledProcessError as e:
             print(f"Failed to install {package}. Error: {e}")
